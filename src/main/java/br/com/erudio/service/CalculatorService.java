585fe8501dd77c5c1a4eea68f3a8cb87755a0db4
João Paulo Souza <joao_p_souza@live.com>
2023-09-19 21:24:40 -0300

    fix: notações dos services

--- src/main/java/br/com/erudio/service/CalculatorService.java
+++ src/main/java/br/com/erudio/service/CalculatorService.java
@@ -1,16 +1,14 @@
 package br.com.erudio.service;
 
-import org.springframework.web.bind.annotation.PathVariable;
-
 import br.com.erudio.exception.UnsuportedMathOperationException;
 
 public interface CalculatorService {
 
-	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo")String numberTwo) throws UnsuportedMathOperationException;
-	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo")String numberTwo) throws UnsuportedMathOperationException;
-	public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo")String numberTwo) throws UnsuportedMathOperationException;
-	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo")String numberTwo) throws UnsuportedMathOperationException;
-	public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo")String numberTwo) throws UnsuportedMathOperationException;
-	public Double raiz(@PathVariable("numberOne") String number) throws UnsuportedMathOperationException;
+	public Double sum(String numberOne,String numberTwo) throws UnsuportedMathOperationException;
+	public Double sub(String numberOne, String numberTwo) throws UnsuportedMathOperationException;
+	public Double mult(String numberOne, String numberTwo) throws UnsuportedMathOperationException;
+	public Double div(String numberOne, String numberTwo) throws UnsuportedMathOperationException;
+	public Double med(String numberOne, String numberTwo) throws UnsuportedMathOperationException;
+	public Double raiz(String number) throws UnsuportedMathOperationException;
 	
 }
